bbf5a9516d304c54c967405f73e54003c81ec636
Maria Clara <maria_clara@maria.clara>
2024-10-21 20:53:40 -0300

    tests

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
+from django.conf.urls import url
+from django.contrib import admin
 
 urlpatterns = [
+    url('login', views.login1, name='login1'),
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.utils import timezone
-from .models import Post
+from .models import *
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
-from django.shortcuts import redirect
+from django.contrib.auth.models import User
+#from django.contrib.auth import logout
 
 def post_edit(request, pk):
      post = get_object_or_404(Post, pk=pk)
@@ -36,7 +39,34 @@ def post_detail (request, pk):
     post = Post.objects.get(pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+@login_required
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def login1(request):
+    #se o metodo do html for POST
+    if request.method == "POST":
+        #captura usuario digitato no input com o name 'usuario'
+        usuario = request.POST.get('usuario')
+        #captura senha digitata no input com o name 'senha'
+        senha = request.POST.get('senha')
+        #autentica se realmente a senha e login esta certo, lembrando que tem que importar 'from django.contrib.auth import authenticate'
+        usu = authenticate(username=usuario, password=senha)
+        #Se tiver tudo certinho com o usuario
+        if usu is not None:
+            #consulta o usuario
+            us = User.objects.filter(username=usuario)
+            for u in us:
+                #verifica o nivel de acesso
+                if u.first_name == "1":
+                    login(request, usu)
+                    return redirect(post_list)
+                else:
+                    #significa que o acesso e diferente e vai pra outra pagina
+                    pass
+        else:
+            return render(request, 'blog/login.html', {'msg': "Falha!"})
+    return render(request, 'blog/login.html',{'msg': ''})
 
